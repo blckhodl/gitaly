a06ad6a81890d38fc7db37e564d3d87cbb544e5a
John Cai <jcai@gitlab.com>
2022-08-04 14:53:33 -0400

    limithandler: Generalize NewConcurrencyLimiter
    Currently, NewConcurrencyLimiter essentially hardcodes a
    ConcurrencyLimiter to be used for per RPC limiting. But, the concurrency
    limiter can be more generalized than that. We will be using it for
    pack-objects limiting.
    
    For this reason, we can generalize the constructor and move the specific
    per RPC limiting code into WithConcurrencyLimiters, since that is used
    specifically with middleware.
    
    Add a NewNullConcurrencyMonitor so we can use it in other packages.
    
    We can also rename newPromMonitor to newPerRPCPromMonitor since it's
    specifically creating metrics for RPCs, and the general sounding
    newPromMonitor is a bit of a misnomer.

--- internal/middleware/limithandler/concurrency_limiter.go
+++ internal/middleware/limithandler/concurrency_limiter.go
@@ -216,7 +216,7 @@ func (c *ConcurrencyLimiter) Limit(ctx context.Context, lockKey string, f Limite
 // NewConcurrencyLimiter creates a new concurrency rate limiter
 func NewConcurrencyLimiter(perKeyLimit, globalLimit int, maxWaitTickerGetter QueueTickerCreator, monitor ConcurrencyMonitor) *ConcurrencyLimiter {
 	if monitor == nil {
-		monitor = &nullConcurrencyMonitor{}
+		monitor = NewNoopConcurrencyMonitor()
 	}
 
 	return &ConcurrencyLimiter{
@@ -284,7 +284,7 @@ func WithConcurrencyLimiters(cfg config.Cfg, middleware *LimiterMiddleware) {
 			limit.MaxPerRepo,
 			limit.MaxQueueSize,
 			newTickerFunc,
-			newPromMonitor("gitaly", limit.RPC, queuedMetric, inProgressMetric,
+			newPerRPCPromMonitor("gitaly", limit.RPC, queuedMetric, inProgressMetric,
 				acquiringSecondsMetric, middleware.requestsDroppedMetric),
 		)
 	}
@@ -298,7 +298,7 @@ func WithConcurrencyLimiters(cfg config.Cfg, middleware *LimiterMiddleware) {
 			func() helper.Ticker {
 				return helper.NewManualTicker()
 			},
-			newPromMonitor("gitaly", replicateRepositoryFullMethod, queuedMetric,
+			newPerRPCPromMonitor("gitaly", replicateRepositoryFullMethod, queuedMetric,
 				inProgressMetric, acquiringSecondsMetric, middleware.requestsDroppedMetric))
 	}
 
--- internal/middleware/limithandler/monitor.go
+++ internal/middleware/limithandler/monitor.go
@@ -20,24 +20,31 @@ type ConcurrencyMonitor interface {
 	Dropped(ctx context.Context, message string)
 }
 
-type nullConcurrencyMonitor struct{}
+type noopConcurrencyMonitor struct{}
 
-func (c *nullConcurrencyMonitor) Queued(ctx context.Context)                           {}
-func (c *nullConcurrencyMonitor) Dequeued(ctx context.Context)                         {}
-func (c *nullConcurrencyMonitor) Enter(ctx context.Context, acquireTime time.Duration) {}
-func (c *nullConcurrencyMonitor) Exit(ctx context.Context)                             {}
-func (c *nullConcurrencyMonitor) Dropped(ctx context.Context, reason string)           {}
+func (c *noopConcurrencyMonitor) Queued(ctx context.Context)                           {}
+func (c *noopConcurrencyMonitor) Dequeued(ctx context.Context)                         {}
+func (c *noopConcurrencyMonitor) Enter(ctx context.Context, acquireTime time.Duration) {}
+func (c *noopConcurrencyMonitor) Exit(ctx context.Context)                             {}
+func (c *noopConcurrencyMonitor) Dropped(ctx context.Context, reason string)           {}
+
+// NewNoopConcurrencyMonitor returns a noopConcurrencyMonitor
+func NewNoopConcurrencyMonitor() ConcurrencyMonitor {
+	return &noopConcurrencyMonitor{}
+}
 
 type promMonitor struct {
 	queuedMetric           prometheus.Gauge
 	inProgressMetric       prometheus.Gauge
 	acquiringSecondsMetric prometheus.Observer
 	requestsDroppedMetric  *prometheus.CounterVec
+
+	acquiringSecondsHistogramVec *prometheus.HistogramVec
 }
 
-// newPromMonitor creates a new ConcurrencyMonitor that tracks limiter
+// newPerRPCPromMonitor creates a new ConcurrencyMonitor that tracks limiter
 // activity in Prometheus.
-func newPromMonitor(
+func newPerRPCPromMonitor(
 	system, fullMethod string,
 	queuedMetric, inProgressMetric *prometheus.GaugeVec,
 	acquiringSecondsMetric prometheus.ObserverVec,
@@ -46,10 +53,10 @@ func newPromMonitor(
 	serviceName, methodName := splitMethodName(fullMethod)
 
 	return &promMonitor{
-		queuedMetric.WithLabelValues(system, serviceName, methodName),
-		inProgressMetric.WithLabelValues(system, serviceName, methodName),
-		acquiringSecondsMetric.WithLabelValues(system, serviceName, methodName),
-		requestsDroppedMetric.MustCurryWith(prometheus.Labels{
+		queuedMetric:           queuedMetric.WithLabelValues(system, serviceName, methodName),
+		inProgressMetric:       inProgressMetric.WithLabelValues(system, serviceName, methodName),
+		acquiringSecondsMetric: acquiringSecondsMetric.WithLabelValues(system, serviceName, methodName),
+		requestsDroppedMetric: requestsDroppedMetric.MustCurryWith(prometheus.Labels{
 			"system":       system,
 			"grpc_service": serviceName,
 			"grpc_method":  methodName,
@@ -84,10 +91,81 @@ func (c *promMonitor) Dropped(ctx context.Context, reason string) {
 	c.requestsDroppedMetric.WithLabelValues(reason).Inc()
 }
 
+func newPromMonitor(
+	keyType string,
+	queuedVec, inProgressVec *prometheus.GaugeVec,
+	acquiringSecondsVec *prometheus.HistogramVec,
+	requestsDroppedVec *prometheus.CounterVec,
+) *promMonitor {
+	return &promMonitor{
+		queuedMetric:                 queuedVec.WithLabelValues(keyType),
+		inProgressMetric:             inProgressVec.WithLabelValues(keyType),
+		acquiringSecondsMetric:       acquiringSecondsVec.WithLabelValues(keyType),
+		requestsDroppedMetric:        requestsDroppedVec,
+		acquiringSecondsHistogramVec: acquiringSecondsVec,
+	}
+}
+
+func (p *promMonitor) Collect(metrics chan<- prometheus.Metric) {
+	p.queuedMetric.Collect(metrics)
+	p.inProgressMetric.Collect(metrics)
+	p.acquiringSecondsHistogramVec.Collect(metrics)
+	p.requestsDroppedMetric.Collect(metrics)
+}
+
+func (p *promMonitor) Describe(descs chan<- *prometheus.Desc) {
+	prometheus.DescribeByCollect(p, descs)
+}
+
 func splitMethodName(fullMethodName string) (string, string) {
 	fullMethodName = strings.TrimPrefix(fullMethodName, "/") // remove leading slash
 	if i := strings.Index(fullMethodName, "/"); i >= 0 {
 		return fullMethodName[:i], fullMethodName[i+1:]
 	}
 	return "unknown", "unknown"
+}
+
+// NewPackObjectsConcurrencyMonitor returns a concurrency monitor for use
+// with limiting pack objects processes
+func NewPackObjectsConcurrencyMonitor(keyType string, latencyBuckets []float64) *promMonitor {
+	acquiringSecondsVec := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "gitaly_pack_objects_acquiring_seconds",
+			Help:    "Histogram of time calls are rate limited (in seconds)",
+			Buckets: latencyBuckets,
+		},
+		[]string{"type"},
+	)
+
+	inProgressVec := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "gitaly_pack_objects_in_progress",
+			Help: "Gauge of number of concurrent in-progress calls",
+		},
+		[]string{"type"},
+	)
+
+	queuedVec := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "gitaly_pack_objects_queued",
+			Help: "Gauge of number of queued calls",
+		},
+		[]string{"type"},
+	)
+
+	requestsDroppedVec := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "gitaly_pack_objects_dropped_total",
+			Help: "Number of requests dropped from the queue",
+		},
+		[]string{"type", "reason"},
+	).MustCurryWith(prometheus.Labels{"type": keyType})
+
+	return newPromMonitor(
+		keyType,
+		queuedVec,
+		inProgressVec,
+		acquiringSecondsVec,
+		requestsDroppedVec,
+	)
 }
